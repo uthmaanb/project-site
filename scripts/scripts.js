81f99a80beb4e7f3abe010de2eb34a71a965aa40
ウスマーン <82204694+uthmaanb@users.noreply.github.com>
2021-09-13 11:03:51 +0200

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -29,3 +29,23 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "flex";
   dots[slideIndex - 1].className += " active";
 }
+
+// count months
+function monthDiff(d1, d2) {
+  let months;
+  months = (d2.getFullYear() - d1.getFullYear()) * 12;
+  months -= d1.getMonth();
+  months += d2.getMonth();
+  return months <= 0 ? 0 : months;
+}
+
+theDate = new Date("April, 6, 2021");
+nowDate = new Date();
+
+count = monthDiff(theDate, nowDate);
+console.log(count);
+
+let counter = document.querySelector(".counter-num");
+console.log(counter);
+
+document.getElementsByClassName("counter-num")[0].innerHTML += `${count}`;
